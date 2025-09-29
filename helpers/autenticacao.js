212a2f2266870c2f9bd51ceb9d2017e0b6505e47
Thais Moreira <thais.dfmoreira@gmail.com>
2025-09-29 19:39:19 -0300

    Adicionando helper para obter token de autenticação e criando teste de transferência

---
+++ helpers/autenticacao.js
@@ -0,0 +1,21 @@
+import http from 'k6/http'
+import { sleep, check } from 'k6'
+const postLogin = JSON.parse(open('../fixtures/postLogin.json'))
+
+export function obterToken() {
+  const url = 'http://localhost:3000/login'
+
+  const payload = JSON.stringify(postLogin)
+
+  const params = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }
+
+  const res = http.post(url, payload, params)
+
+  return res.json('token')
+}
+
+
