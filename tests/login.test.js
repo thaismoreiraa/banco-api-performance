9f0e764976133be09cd579a3461caa45af7fee14
Thais Moreira <thais.dfmoreira@gmail.com>
2025-09-29 16:43:08 -0300

    Adicionando o arquivo inicial com teste de login

---
+++ tests/login.test.js
@@ -0,0 +1,36 @@
+import http from 'k6/http'
+import { sleep, check } from 'k6'
+
+export const options = {
+  iterations: 50,
+  thresholds: {
+    http_req_duration: ['p(90)<10', 'max<1'],
+    http_req_failed: ['rate<0.01']
+  },
+}
+
+export default function () {
+  const url = 'http://localhost:3000/login'
+
+  const payload = JSON.stringify({
+    username: 'julio.lima',
+    senha: '123456',
+  })
+
+  const params = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }
+
+  const res = http.post(url, payload, params)
+
+  check(res, {
+    'Validar que o Status é 200': (r) => r.status === 200,
+    'Validar que o Token é string': (r) => typeof (r.json().token) == 'string'
+  })
+
+  sleep(1)
+
+}
+
